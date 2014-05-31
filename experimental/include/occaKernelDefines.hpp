e24c045165b93ef0df4fa31d8e2db2da412098de
dsm5 <dsm5@kikai>
2014-05-31 13:31:14 -0500

    Updated makefile to check for define updates

--- experimental/include/occaKernelDefines.hpp
+++ experimental/include/occaKernelDefines.hpp
@@ -1,5 +1,5 @@
 namespace occa {
     extern char occaOpenMPDefines[4874];
-    extern char occaOpenCLDefines[3161];
+    extern char occaOpenCLDefines[3162];
     extern char occaCUDADefines[2577];
 }
